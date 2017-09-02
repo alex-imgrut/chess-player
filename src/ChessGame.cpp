803e29c068042ec3b47aa2e23c222801e3945751
alex-imgrut <alex.imgrut@hotmail.com>
2017-09-02 17:17:36 +0200

    [AIM] Implemented tokenizer in ChessGame class

--- src/ChessGame.cpp
+++ src/ChessGame.cpp
@@ -1,42 +1,282 @@
 #include "ChessGame.h"
+#include <cstddef>
+#include <ctype.h>
 #include <iostream>
-#include <sstream>
+#include <stdexcept>
+#include <regex>
+#include <queue>
 
+// --------------------------------------------------------------------------------------------------------------------
+// Lexeme and token types
+// --------------------------------------------------------------------------------------------------------------------
+enum lexemeType { UNDEF, STRING, BRACE_COMMENT, SEMICOLON_COMMENT, SYMBOL };
+enum tokenType { OPEN_BRACKET, CLOSE_BRACKET, TAG_NAME, TAG_VALUE, COMMENT, MOVE_NUMBER, MOVE, RESULT };
+struct token {
+    tokenType type;
+    std::string value;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+// Print token
+// Helper function to debug (cout print)
+// --------------------------------------------------------------------------------------------------------------------
+static std::string token2string(token token) {
+    std::string string = "type [";
+    switch (token.type) {
+        case OPEN_BRACKET:
+            string += "OPEN_BRACKET";
+            break;
+        case CLOSE_BRACKET:
+            string += "CLOSE_BRACKET";
+            break;
+        case TAG_NAME:
+            string += "TAG_NAME";
+            break;
+        case TAG_VALUE:
+            string += "TAG_VALUE";
+            break;
+        case COMMENT:
+            string += "COMMENT";
+            break;
+        case MOVE_NUMBER:
+            string += "MOVE_NUMBER";
+            break;
+        case MOVE:
+            string += "MOVE";
+            break;
+        case RESULT:
+            string += "RESULT";
+            break;
+        default:
+            string += "UNKNOWN";
+    }
+    string += "], value [" + token.value + "]";
+    return string;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Error formatting function
+// --------------------------------------------------------------------------------------------------------------------
+static std::string formatErrorMessage(unsigned int lineNumber, unsigned int charPosition, std::string error, std::string diagnostic) {
+    return error + " at line [" + std::to_string(lineNumber) + "] position [" + std::to_string(charPosition) + "], " + diagnostic;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Evaluation function
+// Maps TOKEN type on LEXEME type
+// --------------------------------------------------------------------------------------------------------------------
+static tokenType evaluate(lexemeType lexemeType, std::string lexemeValue) {
+
+    // Regexes used to validate and categorize symbols
+    std::regex move("^(O-O|O-O-O|[BKNPQR]?[a-h]?[1-8]?x?[a-h][1-8](=[BKNPQR])?[+#]?)$");
+    std::regex moveNumber("^\\d+\\.+$");
+    std::regex tagName("^\\w+$");
+    std::regex result("^(0-1|1-0|1/2-1/2)$");
+
+    tokenType tokenType;
+    switch (lexemeType) {
+
+        // For delimited tokens, lexeme type => token type is univoque
+        case STRING:
+            tokenType = TAG_VALUE;
+            break;
+        case BRACE_COMMENT:
+        case SEMICOLON_COMMENT:
+            tokenType = COMMENT;
+            break;
+
+        // For symbols, lexeme type => token type mapping depends on lexeme value
+        case SYMBOL:
+            if (std::regex_match(lexemeValue, result)) {
+                tokenType = RESULT;
+            }
+            else if (std::regex_match(lexemeValue, moveNumber)) {
+                tokenType = MOVE_NUMBER;
+            }
+            else if (std::regex_match(lexemeValue, move)) {
+                tokenType = MOVE;
+            }
+            else if (std::regex_match(lexemeValue, tagName)) {
+                tokenType = TAG_NAME;
+            }
+            else {
+                throw std::runtime_error("Unknown token [" + lexemeValue + "]");
+            }
+            break;
+        default:
+            throw std::runtime_error("Unknown token [" + lexemeValue + "]");
+    }
+    return tokenType;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// Tokenizer function
+// Standard: Portable Game Notation Specification and Implementation Guide (revised: 1994.03.12)
+// --------------------------------------------------------------------------------------------------------------------
+static std::queue<token> tokenize(std::string text) {
+
+    // Variable used to track current character's position (needed for error messages)
+    unsigned int lineNumber = 1;
+    unsigned int charPosition = 0;
+
+    // Variables and types used for text scanning
+    lexemeType currentLexeme = UNDEF;
+    token currentToken;
+    std::regex rSymbolCharacter("^[\\w\\+\\-\\.:#=/]$");
+
+    // Variable used to store execution result
+    std::queue<token> tokenQueue;
+
+    for (unsigned int i = 0; i < text.size(); i++) {
+
+        charPosition++;
+
+        // PGN text shall be made of ASCII characters only (not printable excluded with exception of space characters)
+        if (!isascii(text[i]) || (!isprint(text[i]) && !isspace(text[i]))) {
+            throw std::runtime_error(formatErrorMessage(lineNumber, charPosition, "syntax error", "invalid character (not ASCII or not printable nor space)"));
+        }
+
+        switch (currentLexeme) {
+
+            case UNDEF: // Outisde of any lexeme, looking for lexeme start
+
+                switch (text[i]) {
+
+                    // Single character tokens detection
+                    case '[':
+                        currentToken.type = OPEN_BRACKET;
+                        currentToken.value = "[";
+                        tokenQueue.push(currentToken);
+                        break;
+
+                    case ']':
+                        currentToken.type = CLOSE_BRACKET;
+                        currentToken.value = "]";
+                        tokenQueue.push(currentToken);
+                        break;
+
+                    case '*':
+                        currentToken.type = RESULT;
+                        currentToken.value = "*";
+                        tokenQueue.push(currentToken);
+                        break;
+
+                    // Delimited tokens detection (string, comments)
+                    case '"':
+                        currentLexeme = STRING;
+                        currentToken.value = "";
+                        break;
+
+                    case '{':
+                        currentLexeme = BRACE_COMMENT;
+                        currentToken.value = "";
+                        break;
+
+                    case ';':
+                        currentLexeme = SEMICOLON_COMMENT;
+                        currentToken.value = "";
+                        break;
+
+                    // Complex tokens detection (symbols, starting with a letter or a digit)
+                    default:
+                        if (isalnum(text[i])) {
+                            currentLexeme = SYMBOL;
+                            currentToken.value = text[i];
+                        }
+                        else if (isspace(text[i])) {
+                            // Do nothing, ignore white spaces
+                        }
+                        else {
+                            throw std::runtime_error(formatErrorMessage(lineNumber, charPosition, "syntax error", "invalid character to start a token"));
+                        }
+                }
+                break;
+
+            case STRING: // Inside a string, looking for end of the string
+                if ((text[i] == '"') && ((i == 0) || (text[i - 1] != '\\'))) {
+                    currentToken.type = evaluate(currentLexeme, currentToken.value);
+                    tokenQueue.push(currentToken);
+                    currentLexeme = UNDEF;
+                }
+                else {
+                    currentToken.value += text[i];
+                }
+                break;
+
+            case BRACE_COMMENT: // Inside a comment, looking for end of the comment
+                if (text[i] == '}') {
+                    currentToken.type = evaluate(currentLexeme, currentToken.value);
+                    tokenQueue.push(currentToken);
+                    currentLexeme = UNDEF;
+                }
+                else {
+                    currentToken.value += text[i];
+                }
+                break;
+
+            case SEMICOLON_COMMENT: // Inside a comment, looking for end of the comment
+                if (text[i] == '\n') {
+                    currentToken.type = evaluate(currentLexeme, currentToken.value);
+                    tokenQueue.push(currentToken);
+                    currentLexeme = UNDEF;
+                }
+                else {
+                    currentToken.value += text[i];
+                }
+                break;
+    
+            case SYMBOL: // Inside a symbol, looking for end of the symbol
+                if (!std::regex_match(std::string(1, text[i]), rSymbolCharacter)) {
+                    currentToken.type = evaluate(currentLexeme, currentToken.value);
+                    tokenQueue.push(currentToken);
+                    currentLexeme = UNDEF;
+                }
+                else {
+                    currentToken.value += text[i];
+                }
+                break;
+
+            default: // Should be impossible to end here
+                throw std::runtime_error(formatErrorMessage(lineNumber, charPosition, "syntax error", "inside unknown token"));
+        }
+        
+        // When a new line character is encountered, increment line number and reset character position
+        if (text[i] == '\n') {
+            lineNumber++;
+            charPosition = 0;
+        }
+    }
+
+    // EOF was reached and last toekn has not ended
+    if (currentLexeme != UNDEF) {
+        throw std::runtime_error(formatErrorMessage(lineNumber, charPosition, "syntax error", "missing terminator ('\"', '}' or '\n'"));
+    }
+
+    return tokenQueue;
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// Temp stuff
+// --------------------------------------------------------------------------------------------------------------------
 // By default, FEN record matches standard starting position
-const std::string ChessGame::scDefaultFenRecord = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+// const std::string ChessGame::scDefaultFenRecord = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
 // Constructor is building a PGN export format object
 // Initialize mandatory tag pairs (STR = Seven Tag Roster) and add tags FEN and SetUp with default values
 ChessGame::ChessGame(std::string pgnString)
 {
-    // Parse pgnString to extract the tag pairs and the move text string
-    // Raise errors in case of syntax errors
-    // Example of PGN string
-    // [tagName "tagValue"], tagName in ASCII
-    /*
-       [Event "F/S Return Match"]
-       [Site "Belgrade, Serbia JUG"]
-       [Date "1992.11.04"]
-       [Round "29"]
-       [White "Fischer, Robert J."]
-       [Black "Spassky, Boris V."]
-       [Result "1/2-1/2"]
-       
-       1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 {This opening is called the Ruy Lopez.}
-       4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 O-O 9. h3 Nb8 10. d4 Nbd7
-       11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5
-       Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4 22. Bxc4 Nb6
-       23. Ne5 Rae8 24. Bxf7 + Rxf7 25. Nxf7 Rxe1 + 26. Qxe1 Kxf7 27. Qe3 Qg5 28. Qxg5
-       hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kf2 Bf5
-       35. Ra7 g6 36. Ra6 + Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6
-       Nf2 42. g4 Bd3 43. Re6 1/2-1/2
-    */
-    
-    // Assumption : there is at most one tag per line
-    std::string line;
-    std::stringstream pgnText = std::stringstream(pgnString);
-    while (std::getline(pgnText, line, '\n')) {
-        // process line (extract tang name and value)
+    try {
+        std::queue<token> tokenQueue = tokenize(pgnString);
+        token token;
+        while (!tokenQueue.empty()) {
+            token = tokenQueue.front();
+            std::cout << "TOKEN " << token2string(token) << std::endl;
+            tokenQueue.pop();
+        }
+    }
+    catch (const std::runtime_error& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
     }
 }
 
--- src/ChessGame.h
+++ src/ChessGame.h
@@ -1,17 +1,10 @@
 #pragma once
-
-#include <map>
 #include <string>
 
 class ChessGame {
-
 private:
     static const std::string scDefaultFenRecord;
-    std::map<std::string, std::string> mTagPairs;
-    std::string mMoveText;
-
 public:
     ChessGame(std::string pgnString);
     std::string toString();
-
 };
